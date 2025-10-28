386ac8f39facc49492269d5db0e12789efe5a29d
Omar-NF <107004923+Omar-NF@users.noreply.github.com>
2025-10-28 12:00:02 -0600

    Configuracion de app.js

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv"; //sirve para leer cosas del .env
+import verbRoutes from "./routes/verbs.js";
+
+dotenv.config(); //configuracion (o lectura) del .env, no nesesita que le pases nada, 
+
+const app = express(); //app es un objeto para usar express
+const PORT = process.env.PORT || 3000; // aqui se dice que el puerto sera igual a el .PORT del .env si no sera 3000
+
+app.use(express.json()); // Esto convierte peticiones JSON a un objeto javascript dentro de req.body
+app.use("/api/verbs", verbRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`); 
+});
+
