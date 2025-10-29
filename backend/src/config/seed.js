25c323be6fa68aeff4bb038cb54b7ca260a41912
omar-nif <omarnicanor8@gmail.com>
2025-10-29 11:57:19 -0600

    Poblado de la base de datos part 1

---
+++ backend/src/config/seed.js
@@ -0,0 +1,6 @@
+import db from "./db.js";
+
+const verbs = [
+    { base_form: "", past_simple: "was/were", past_participle: "" },
+    { base_form: "", past_simple: "was/were", past_participle: "" },
+]
